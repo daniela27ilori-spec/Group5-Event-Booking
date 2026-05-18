06ff334f4bc031c11a1865cfae8f494aa13642eb
omugbe ewomazino <ewomazino.omugbe@pau.edu.ng>
2026-05-18 23:34:49 +0100

    Added a dashboard page with the nagivation bar and a UI text

---
+++ client/app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+import Navbar from "../../components/Navbar";
+
+export default function Dashboard() {
+    return(
+        <>
+      <Navbar  />
+      <div>
+        <h1 style={{ fontSize: "30px" }}>
+  Welcome to your Dashboard!
+</h1>
+        </div>  
+        
+        
+        
+        </>
+    
+    )
+}
+
